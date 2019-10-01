e978f4b76894329803e9fc248510be57cc70b6d9
utooley <utooley@gmail.com>
2019-10-01 14:34:44 -0400

    added second chunk of subject ppc

--- utilities/remove_TRs.sh
+++ utilities/remove_TRs.sh
@@ -4,20 +4,21 @@
 #$ -V
 
 if [ $# -eq 0 ]; then
-echo "USAGE: remove_TRs.sh <sub_id> <ses> <scan> <num of TRS to keep>
+echo "USAGE: remove_TRs.sh <sub_id> <ses> <rest scan number> <num of TRS to keep>
 
-Example: remove_TRs.sh CBPDxxx xxx 16
-TRs in the CBPD dataset are 2s for resting-state data. "
+Example: remove_TRs.sh CBPD0173 01 02 140
+Keeps only the first 140 TRs from the second resting-state scan in the first session for CBPD0173.
+This removes TRs from the resting-state scans only. In the CBPD dataset, TRs are 2s for resting-state data. "
 exit
 fi
 
 sub=${1}
 ses=${2}
-scan=${3}
+scannum=${3}
 num=${4}
 
-num=211
-scan=sub-CBPD0173_ses-01_task-rest_run-01_bold.nii.gz
+data_dir=/data/picsl/mackey_group/CBPD/CBPD_bids
+scan=${data_dir}/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-rest_run-${scannum}_bold.nii.gz
 #AFNI version is an alternative
 #3dcalc -a ${scan}[0-"$num"] -expr 'a' -prefix new.nii.gz
 
