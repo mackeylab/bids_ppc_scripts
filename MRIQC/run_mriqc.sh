363e38883112730ce7ba4e7b4d963966a89c120a
utooley <utooley@gmail.com>
2019-10-01 15:59:25 -0400

    added second subj script

---
+++ MRIQC/run_mriqc.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#$ -cwd
+#$ -V
+
+MACKEY_HOME=/data/picsl/mackey_group/
+#BIDS_folder=/data/picsl/mackey_group/BPD/niftis
+BIDS_folder=/data/picsl/mackey_group/BPD/CBPD_bids
+working_dir=/data/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/data/mriqc_working
+subject=${1}
+tools_dir=${MACKEY_HOME}/tools/singularity
+output_dir=${BIDS_folder}/derivatives/mriqc_fd_2_mm
+echo $subject
+
+unset PYTHONPATH;
+singularity run --cleanenv -B ${BIDS_folder}:/mnt ${tools_dir}/mriqc-0.14.2.simg \
+/mnt/ /mnt/derivatives/mriqc_fd_2_mm \
+participant \
+-w /tmp \
+--participant_label ${subject} \
+--fd_thres 2 \
+--no-sub \
+--n_procs 10 \
+
+#must bind to a folder that already exists in the container, and must point to data dir not subject dir
+
+#MRIQC group command
+
+MACKEY_HOME=/data/picsl/mackey_group/
+#BIDS_folder=/data/picsl/mackey_group/BPD/niftis
+BIDS_folder=/data/picsl/mackey_group/BPD/CBPD_bids
+tools_dir=${MACKEY_HOME}/tools/singularity
+output_dir=${BIDS_folder}/derivatives/mriqc_fd_.5_mm
+
+unset PYTHONPATH;
+singularity run --cleanenv -B ${BIDS_folder}:/mnt ${tools_dir}/mriqc-0.14.2.simg \
+/mnt/ /mnt/derivatives/mriqc_fd_.5_mm \
+group \
+-w ${TMP} \
+
+
+#don't have the working dir be a mounted directory or you'll get errors in the logs about busy resources
+#must bind to a folder that already exists in the container, and must point to data dir not subject dir
---
+++ new_subj_second.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+#$ -cwd
+#$ -V
+
+if [ $# -eq 0 ]; then
+echo "USAGE: new_subj_second.sh <sub_id> <ses> <T1 for freesurfer> <T2 for freesurfer>
+
+Example: new_subj_second.sh CBPDxxx xxx
+This runs MRIQC on this subject, and adds their quality metrics to the MRIQC group output.
+Then, it runs Freesurfer using the selected T1 and T2, including hippocampal subfields.
+Finally, it start fMRIprep running, which should detect precomputed freesurfer inputs."
+exit
+fi
+
+${sub}=${1} #CBPDxxxx
+${ses}=${2} #01,02,03
+
+######
+###Run MRIQC###
+######
+
+echo Running MRIQC for ${sub}
+
+#do Heudiconv
+
+echo Finished MRIQC for ${sub}
+
+
+######
+###Collate MRIQC group output again###
+######
+
+echo Adding ${sub} to MRIQC group files
+
+#do it
+
+echo Finished adding ${sub} to MRIQC group files
+
+######
+###Run Freesurfer with hippocampal subfields, selected T1###
+######
+
+#this may change to use both the T1 and T2? Probably this should be the default.
+echo Running Freesurfer with hipp subfields for ${sub}
+
+#do it
+
+echo Finished running Freesurfer with hipp subfields for ${sub}
+
+######
+###Run fMRIprep###
+######
+
+#with precomputed Freesurfer
+#we may in the future be able to select which T1 and T2 get fed to fmriprep, without having to .bidsignore them
+#also testing using our fieldmaps for distortion correction of EPIs and not just DWIs
+echo Running fMRIprep for ${sub}
+
+#do it
+
+echo Finished running fMRIprep for ${sub}
