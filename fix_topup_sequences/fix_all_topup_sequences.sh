dd780e25acede9193787994db3f65aa6069caeea
utooley <utooley@gmail.com>
2020-02-28 10:43:03 -0500

    new names for topup sequences specific to dti

--- fix_topup_sequences/fix_all_topup_sequences.sh
+++ fix_topup_sequences/fix_all_topup_sequences.sh
@@ -27,18 +27,18 @@ do
     #cd ${BIDS_dir}/${sub}/${ses}
     fieldmap_orig=${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_run-01_epi.nii.gz #the original fieldmap
     echo ${fieldmap_orig}
-    topup_jpos=${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_dir-PA_run-01_epi.nii.gz
-    topup_jneg=${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_dir-AP_run-01_epi.nii.gz
+    topup_jpos=${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_acq-dwi_dir-PA_run-01_epi.nii.gz
+    topup_jneg=${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_acq-dwi_dir-AP_run-01_epi.nii.gz
     #the fieldmap is acquired in the j direction
     if [ -e ${topup_jpos} ]; then
       echo 'files exist already' #we already fixed the files
     elif 3dcalc -a ${fieldmap_orig}[0] -expr '1*a' -prefix ${topup_jpos} ; then  #take only the first vol of TOPUP scan
-      mv ${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_run-01_epi.json ${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_dir-PA_epi.json #rename the TOPUP json
+      mv ${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_run-01_epi.json ${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_acq-dwi_dir-PA_epi.json #rename the TOPUP json
       #the dwi scan is acquired in the -j direction, take the last scan of the dwi, which is b=0, before TOPUP
       dwi=${BIDS_dir}/${sub}/${ses}/dwi/${sub}_${ses}_run-01_dwi.nii.gz
       3dcalc -a ${dwi}[$] -expr '1*a' -prefix ${topup_jneg} #take only the last vol of DWI
-      cp ${BIDS_dir}/${sub}/${ses}/dwi/${sub}_${ses}_run-01_dwi.json ${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_dir-AP_epi.json #copy the dwi json
-      if [ ! -w "${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_dir-PA_epi.json" ] #if file is not writable then
+      cp ${BIDS_dir}/${sub}/${ses}/dwi/${sub}_${ses}_run-01_dwi.json ${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_acq-dwi_dir-AP_epi.json #copy the dwi json
+      if [ ! -w "${BIDS_dir}/${sub}/${ses}/fmap/${sub}_${ses}_acq-dwi_dir-PA_epi.json" ] #if file is not writable then
       then
         echo 'file is not writable'
         chmod 775 ${BIDS_dir}/${sub}/${ses}/fmap/* #change the permissions so you can write into the .json files later
--- fix_topup_sequences/fix_one_sub_topup_sequence.sh
+++ fix_topup_sequences/fix_one_sub_topup_sequence.sh
@@ -20,19 +20,18 @@ BIDS_dir=${3}
 cd ${BIDS_dir}/${sub}/${ses}
 fieldmap_orig=fmap/${sub}_${ses}_run-01_epi.nii.gz #the original fieldmap
 echo ${fieldmap_orig}
-topup_jpos=fmap/${sub}_${ses}_dir-PA_run-01_epi.nii.gz
-topup_jneg=fmap/${sub}_${ses}_dir-AP_run-01_epi.nii.gz
+topup_jpos=fmap/${sub}_${ses}_acq-dwi_dir-PA_run-01_epi.nii.gz
+topup_jneg=fmap/${sub}_${ses}_acq-dwi_dir-AP_run-01_epi.nii.gz
 #the fieldmap is acquired in the j direction
 3dcalc -overwrite -a ${fieldmap_orig}[0] -expr '1*a' -prefix ${topup_jpos} #take only the first vol of TOPUP
-mv fmap/${sub}_${ses}_run-01_epi.json fmap/${sub}_${ses}_dir-PA_epi.json #rename the TOPUP json
+mv fmap/${sub}_${ses}_run-01_epi.json fmap/${sub}_${ses}_acq-dwi_dir-PA_epi.json #rename the TOPUP json
 #the dwi scan is acquired in the -j direction, take the last scan of the dwi, which is b=0, before TOPUP
 dwi=dwi/${sub}_${ses}_run-01_dwi.nii.gz
 3dcalc -overwrite -a ${dwi}[$] -expr '1*a' -prefix ${topup_jneg} #take only the last vol of DWI
-cp dwi/${sub}_${ses}_run-01_dwi.json fmap/${sub}_${ses}_dir-AP_epi.json #copy the dwi json
+cp dwi/${sub}_${ses}_run-01_dwi.json fmap/${sub}_${ses}_acq-dwi_dir-AP_epi.json #copy the dwi json
 chmod 755 fmap/* #change the permissions so you can write into the .json files later
 
 rm fmap/${sub}*.bval
 rm fmap/${sub}*.bvec
 rm ${fieldmap_orig} -f
 cd ../../
-
