570684604b44ce94c55e267d7d4116636b7c1506
utooley <utooley@gmail.com>
2020-04-27 17:20:01 -0400

    changed order of parsing mriqc columns

--- utilities/parse_mriqc_cols.py
+++ utilities/parse_mriqc_cols.py
@@ -21,7 +21,7 @@ output_filename= join(MRIQC_dir,"MRIQC_filtered_columns_"+threshold+"mm.csv")
 
 df = pd.read_csv(join(MRIQC_dir,"group_bold.tsv"),
     sep = '\t')
-df=df[['bids_name', 'fd_mean', 'fd_num', 'fd_perc', 'size_t']]
-df = df.rename(columns={'fd_mean':'MRIQC mean FD', 'fd_num':'MRIQC # outliers > '+threshold+' mm',
-    'fd_perc':'MRIQC % outliers > '+threshold+' mm', 'size_t':'MRIQC # of vols'})
+df=df[['bids_name', 'size_t','fd_mean', 'fd_num', 'fd_perc']]
+df = df.rename(columns={'size_t':'MRIQC # of vols', 'fd_mean':'MRIQC mean FD', 'fd_num':'MRIQC # outliers > '+threshold+' mm',
+    'fd_perc':'MRIQC % outliers > '+threshold+' mm'})
 df.to_csv(output_filename)
