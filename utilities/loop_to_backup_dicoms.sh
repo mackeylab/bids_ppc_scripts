92a6b27e4d8b93cfaefc66946611b023c9146e0c
utooley <utooley@gmail.com>
2021-06-24 14:54:06 -0400

    update to pull from CUBIC not CfN

--- utilities/loop_to_backup_dicoms.sh
+++ utilities/loop_to_backup_dicoms.sh
@@ -1,5 +1,6 @@
 #may need to run several times if the link to the cluster gets broken
 #run from your local computer with the hard drive plugged in, will need to enter password each time
+#You will need to change the username from TooleyU to your CUBIC username
 set -euo pipefail
 
 declare -a subs=(CBPD0162_2)
@@ -7,7 +8,7 @@ declare -a subs=(CBPD0162_2)
 for i in "${subs[@]}"
 do
 #longitudinal
-rsync -prltD --chmod=Dug+rwx,Dg+s,Fug+rw,o-rwx utooley@chead:/data/picsl/mackey_group/BPD/dicoms/${i} /Volumes/Mackey\ Dicoms/Child_Longitudinal_Dicoms/
+rsync -prltD --chmod=Dug+rwx,Dg+s,Fug+rw,o-rwx Tooleyu@cubic-login.uphs.upenn.edu:/cbica/projects/cbpd_main_data/dicoms/${i} /Volumes/Mackey\ Dicoms/Child_Longitudinal_Dicoms/
 #non-longitudinal
-#rsync -prltD --chmod=Dug+rwx,Dg+s,Fug+rw,o-rwx utooley@chead:/data/picsl/mackey_group/BPD/dicoms/${i} /Volumes/Mackey\ Dicoms/Child_Dicoms/
+#rsync -prltD --chmod=Dug+rwx,Dg+s,Fug+rw,o-rwx Tooleyu@cubic-login.uphs.upenn.edu:/cbica/projects/cbpd_main_data/dicoms/${i} /Volumes/Mackey\ Dicoms/Child_Dicoms/
 done
