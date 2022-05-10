48ef1d627c78713696e169fbd472aac628a3ab02
Lourdes Delgado Reyes <45011826+lmarielledr@users.noreply.github.com>
2022-05-10 15:58:48 -0400

    Update flywheel_download_script.sh
    updating the notes

--- utilities/flywheel_download_script.sh
+++ utilities/flywheel_download_script.sh
@@ -24,21 +24,21 @@ tar -xvf ${SUBJECT_ID}.tar
 ls
 
 # if the contents of the folder are a .tar file and a folder named scitran, use the second move. otherwise use the first.
+
 # move all dicoms that aren't zipped up to the top level folder
-
-## BE SURE TO CHECK WHICH ONE YOU NEED TO RUN. DO NOT RUN THEM BOTH
 mv unknown/Unsorted/${SUBJECT_ID}/**/*/*dcm .
-mv scitran/unknown/Unsorted/${SUBJECT_ID}/**/*/*dcm .
+# mv scitran/unknown/Unsorted/${SUBJECT_ID}/**/*/*dcm .
 
-## NOTE! If for some reason the subject was registered multiple times, there may be multiple folders under
+#NOTE! If for some reason the subject was registered multiple times, there may be multiple folders under
 # unknown/Unsorted/${SUBJECT_ID} that will need to be sorted through. If this is the case,
 # it may be helpful to only mv and unzip the dicoms you want to use from the folders
 # in the folder structure, and delete or hide the others from heudiconv.
 
 # You can also investigate using --grouping all when converting using heudiconv
 
-# this unzips all the dicom folders to the top-level subject folder in /dicoms, though they will still be nested.
-# find /cbica/projects/cbpd_main_data/dicoms/${SUBJECT_ID} -name "*.zip" | while read filename; do unzip -o -d "`basename -s .zip "$filename"`" "$filename"; done;
+#this unzips all the dicom folders to the top-level subject folder in /dicoms, though they will still be nested.
+#find /cbica/projects/cbpd_main_data/dicoms/${SUBJECT_ID} -name "*.zip" | while read filename; do unzip -o -d "`basename -s .zip "$filename"`" "$filename"; done;
+# commenting ^ out since we are using the version below. DO NOT RUN BOTH, IT WILL BREAK THE PROCESS.
 
 # Lourdes' update 23-mar-2022
 # this unzips all the dicom folders to the top-level subject folder in /dicoms, though they will still be nested and
