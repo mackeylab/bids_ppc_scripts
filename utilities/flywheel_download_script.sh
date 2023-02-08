c4af72f0fc4ec4efaef5801adaa8a1e4e2ca86cf
isisco <106333481+isisco@users.noreply.github.com>
2023-02-08 15:42:37 -0500

    Update flywheel_download_script.sh

--- utilities/flywheel_download_script.sh
+++ utilities/flywheel_download_script.sh
@@ -5,7 +5,7 @@ sudo -u cbpdmaindata bash #log in to the cbpdmaindata project user
 
 CBPD_DATA=/cbica/projects/cbpd_main_data/
 SCRIPTS_DIR=/cbica/projects/cbpd_main_data/code/bids_ppc_scripts
-APIKEY=#put your API Key from Flywheel here
+APIKEY= #put your API Key from Flywheel here
 SUBJECT_ID=CBPD0199_2
 
 fw login $APIKEY
@@ -14,7 +14,7 @@ cd $CBPD_DATA/dicoms/
 mkdir ${SUBJECT_ID}; cd ${SUBJECT_ID} #make a new subject folder
 
 fw download unknown/Unsorted/${SUBJECT_ID}
-# you will get prompted for the size being okay-yes/no? Hit yes!
+#you will get prompted for the size being okay-yes/no? Hit yes!
 
 tar -xvf ${SUBJECT_ID}.tar
 
@@ -22,12 +22,12 @@ tar -xvf ${SUBJECT_ID}.tar
 # for some reason the recently downloaded and unzipped tar path changed. it is now 'scitran/unknown/Unsorted'
 # check the path for the unzipped files so you can later use the appropriate mv command
 ls
+#move all dicoms that aren't zipped up to the top level folder
+#CHECK THE PATH. may need to use 'scitran/' version
+#if the contents of the folder are a .tar file and a folder named scitran, use the SECOND move. otherwise use the first.
 
-# if the contents of the folder are a .tar file and a folder named scitran, use the second move. otherwise use the first.
-
-# move all dicoms that aren't zipped up to the top level folder
-mv unknown/Unsorted/${SUBJECT_ID}/**/*/*dcm .
-# mv scitran/unknown/Unsorted/${SUBJECT_ID}/**/*/*dcm .
+#mv unknown/Unsorted/${SUBJECT_ID}/**/*/*dcm .
+mv scitran/unknown/Unsorted/${SUBJECT_ID}/**/*/*dcm .
 
 #NOTE! If for some reason the subject was registered multiple times, there may be multiple folders under
 # unknown/Unsorted/${SUBJECT_ID} that will need to be sorted through. If this is the case,
@@ -40,14 +40,14 @@ mv unknown/Unsorted/${SUBJECT_ID}/**/*/*dcm .
 #find /cbica/projects/cbpd_main_data/dicoms/${SUBJECT_ID} -name "*.zip" | while read filename; do unzip -o -d "`basename -s .zip "$filename"`" "$filename"; done;
 # commenting ^ out since we are using the version below. DO NOT RUN BOTH, IT WILL BREAK THE PROCESS.
 
-# Lourdes' update 23-mar-2022
-# this unzips all the dicom folders to the top-level subject folder in /dicoms, though they will still be nested and
-# now it names the folder according to the name of the sequence (taken from the folder where the dicom is sitting at)
+# update 11-mar-2022 by LDR
+# lourdes' fix to give the folder name the sequence info e.g.
+# for the AAHead_Scout sequences it will name the folder 'AAHead_Scout/dicom_file_name/*.dcm' and will put the uzipped files in there
 find /cbica/projects/cbpd_main_data/dicoms/${SUBJECT_ID} -name "*.zip" | while read filename; do unzip -o -d "`basename -s .zip "$(dirname "$filename")"`" "$filename"; done;
 
 #run the first processing script to convert the dicoms to nifti!
 
-qsub ${SCRIPTS_DIR}/new_subj_first.sh ${SUBJECT_ID} /cbica/projects/cbpd_main_data/CBPD_bids
+qsub ${SCRIPTS_DIR}/new_subj_first_CAREER.sh ${SUBJECT_ID} /cbica/projects/cbpd_main_data/CBPD_bids
 
 # Remember to record in CBPD Scanning Data google sheet that you have copied the dicoms over,
 # and any notes on quality or issues.
@@ -59,6 +59,3 @@ qsub ${SCRIPTS_DIR}/new_subj_first.sh ${SUBJECT_ID} /cbica/projects/cbpd_main_da
 # If dicoms are not being converted properly with heudiconv to nifti format,
 # check the heuristic is up-to-date for any new sequences that have been added
 # for HBCD or otherwise (bids_ppc_scripts/heudiconv/heuristic.py)
-
-# TROUBLESHOOTING
-# If the anat/ folder is missing after the new_subj_first.sh, something went wrong. make sure to delete the dicoms and the subject's CBPD_bids/.heudiconv/$sub/ folders before trying it again.
